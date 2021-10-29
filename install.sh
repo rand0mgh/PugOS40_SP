5e6f792729784725f2e69ac009b6a90870ee8580
rand0mgh <65503947+rand0mgh@users.noreply.github.com>
2021-10-29 09:51:21 +0100

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,12 @@
+sudo apt install xubuntu-desktop
+sudo apt install lxpanel
+sudo apt-get install cairo-dock cairo-dock-plug-ins.
+sudo apt remove xfce4-panel
+sudo apt install git
+cd ~
+git clone https://github.com/vinceliuice/Fluent-gtk-theme.git
+mkdir .themes
+sudo apt install zenity
+echo "!! PLEASE ALSO SET CAIRO AND LXPANEL TO RUN ON STARTUP !!"
+zenity --info --text="Please install the "Oreo" icon theme from https://www.gnome-look.org/p/1533750/"
+zenity --info --text="Thanks for installing the PugOS 40 beta"
